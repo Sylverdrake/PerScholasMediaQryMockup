d8e84620eb2939ec1d9d33eb2a61c76aa36d98b8
Sylverdrake <126032225+Sylverdrake@users.noreply.github.com>
2023-06-16 17:22:55 -0400

    Now with toggles!

---
+++ app.js
@@ -0,0 +1,9 @@
+const buttonColor = () => {
+    let banner = document.querySelector('.linkTxt')
+    banner.classList.toggle('changeform')
+}
+
+const buttonColor2 = () => {
+    let banner = document.querySelector('.linkTxt2')
+    banner.classList.toggle('changeform')
+}
